9041cd959618594644b4b05d28c866ec6c015b88
Jean Hommet <hommet@lpccaen.in2p3.fr>
2017-03-16 17:23:48 +0100

    [BxRabbitMQ] MANAGEMENT fix multi root code

--- src/rabbitmq/rabbit_mgr.cc
+++ src/rabbitmq/rabbit_mgr.cc
@@ -317,10 +317,10 @@ namespace rabbitmq {
    bool _request_test_ (curlpp::Easy & request_,
                         std::string  & response_)
    {
-      std::stringstream ss;
+      std::ostringstream os;
       try {
-         ss << request_;
-         response_ = ss.str ();
+         os << request_;
+         response_ = os.str ();
          return true;
       } catch (std::exception & x) {
          response_ = x.what ();
@@ -368,9 +368,9 @@ namespace rabbitmq {
    {
       std::string str_response;
       try {
-         std::stringstream ss;
-         ss << request_;
-         str_response = ss.str ();
+         std::ostringstream os;
+         os << request_;
+         str_response = os.str ();
          error_       = error_response::response_ok ();
       } catch (std::exception & x) {
          std::cerr << "ERROR: " << x.what() << std::endl;
@@ -379,13 +379,13 @@ namespace rabbitmq {
          return false;
       }
       try {
-         std::stringstream ss (str_response);
-         jsontools::load (ss, response_);
+         std::istringstream is (str_response);
+         jsontools::load (is, response_);
          return true;
       } catch (...) {
          //std::clog << "\n== ERROR RESPONSE ==> " << str_response << std::endl;
-         std::stringstream ss (str_response);
-         jsontools::load (ss, error_);
+         std::istringstream is (str_response);
+         jsontools::load (is, error_);
          return false;
       }
    }
@@ -394,8 +394,8 @@ namespace rabbitmq {
    std::string _vhost_name_ (const std::string & vhost_code_)
    {
       std::string name (vhost_code_);
-      size_t      pos  = name.find ("%2f");
-      if (pos != std::string::npos) {
+      size_t      pos;
+      while ((pos = name.find ("%2f")) != std::string::npos) {
          name.replace (pos, 3, "/");
       }
       return name;
@@ -405,11 +405,13 @@ namespace rabbitmq {
    std::string _vhost_code_ (const std::string & vhost_name_)
    {
       std::string code (vhost_name_);
-      if (code [0] == '/') {
-         code.replace (0, 1, "%2f");
+      size_t      pos;
+      while ((pos = code.find ("/")) != std::string::npos) {
+         code.replace (pos, 1, "%2f");
       }
       return code;
    }
+
 
 
    std::string _to_string_ (const bool b)
